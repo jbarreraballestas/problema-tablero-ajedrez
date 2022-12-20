52fe0f8092df92aca8aca4fd8efaf4c22e9cba7c
JBarreraBallestas <jbarreraballestas@correo.unicordoba.edu.co>
2022-12-20 09:06:46 -0500

    Classes for black and white, added cursor pointer

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ let tablero;
 let current = BigInt(1);
 let total = BigInt(0);
 window.addEventListener('load', function (param) {
-
+  let black = true;
   let b = document.querySelector('body');
   tablero = Array(8 * 8).fill(0);
   tablero.forEach((element, index) => {
     current = index == 0 ? current : current * 2n;
     tablero[index] = current;
     total += current;
+    casilla = index + 1;
     let pieza = document.createElement('div');
-    pieza.id = 'pieza' + (index + 1);
-    pieza.dataset.id = (index + 1);
+    pieza.id = 'pieza' + casilla;
+    pieza.dataset.id = casilla;
     pieza.dataset.total = total;
-    pieza.classList = 'flex items-center justify-center border border-solid border-black';
+    let currentclass = black ? 'bg-black text-white' : '';
+    black = !black;
+    if (casilla %8==0) {
+      black = !black;
+    }
+    pieza.classList = 'flex items-center justify-center cursor-pointer ' + currentclass;
     pieza.innerHTML = current.toLocaleString();
     pieza.onclick = function (param) { alert(`El total acumulado hasta la casilla ${param.target.dataset.id} es ${BigInt(param.target.dataset.total).toLocaleString()}`); };
     b.appendChild(pieza);
